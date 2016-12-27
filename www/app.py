b610a0d412d6c5df002f115cb9e1cf37f6574585
saka <asclepius1993@gmail.com>
2016-12-26 20:53:14 -0500

    basic framework

---
+++ www/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Respnse(body=b'<h1>Awesome</h1>',content_type='text/plain')
+
+
+async def init(loop):
+    app = web.Application(loop=loop)#use the loop from eventloop
+    app.router.add_route('GET','/',index)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
+
